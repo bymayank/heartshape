5df05ac3d8dac5748372d80bce38cfd556dc71e4
Mayank Garg <67113424+bymayank@users.noreply.github.com>
2026-02-05 19:51:53 +0530

    Update TextFooter.tsx

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -11,14 +11,14 @@ export default function TextFooter() {
     <>
       {/* Left Text */}
       <h1
-        className={`absolute left-10 top-1/2 transform -translate-y-1/2 text-[#cfaa96] text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
+        className={`absolute left-10 top-1/2 transform -translate-y-1/2 text-[#cfaa96] text-3xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
       >
         Match<br /> the photo pairs
       </h1>
 
       {/* Right Text */}
       <h1
-        className={`absolute right-10 top-1/2 transform -translate-y-1/2 text-[#cfaa96] text-4xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
+        className={`absolute right-10 top-1/2 transform -translate-y-1/2 text-[#cfaa96] text-3xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
       >
         to reveal <br /> the surprise
       </h1>
