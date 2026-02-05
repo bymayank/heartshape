6eb45b5d3d1b93833b5a3ec6298085907f906cdd
Mayank Garg <67113424+bymayank@users.noreply.github.com>
2026-02-05 19:39:16 +0530

    Update ValentinesProposal.tsx

--- src/components/ValentinesProposal.tsx
+++ src/components/ValentinesProposal.tsx
@@ -11,42 +11,42 @@ const playfairDisplay = Playfair_Display({
 
 // 36 images
 const images = [
-  "/game-photos/1.avif",
-  "/game-photos/2.avif",
-  "/game-photos/3.avif",
-  "/game-photos/4.avif",
-  "/game-photos/5.avif",
-  "/game-photos/6.avif",
-  "/game-photos/7.avif",
-  "/game-photos/8.avif",
-  "/game-photos/9.avif",
-  "/game-photos/10.avif",
-  "/game-photos/11.avif",
-  "/game-photos/12.avif",
-  "/game-photos/13.avif",
-  "/game-photos/14.avif",
-  "/game-photos/15.avif",
-  "/game-photos/16.avif",
-  "/game-photos/17.avif",
-  "/game-photos/18.avif",
-  "/game-photos/19.avif",
-  "/game-photos/20.avif",
-  "/game-photos/21.avif",
-  "/game-photos/22.avif",
-  "/game-photos/23.avif",
-  "/game-photos/24.avif",
-  "/game-photos/25.avif",
-  "/game-photos/26.avif",
-  "/game-photos/27.avif",
-  "/game-photos/28.avif",
-  "/game-photos/29.avif",
-  "/game-photos/30.avif",
-  "/game-photos/31.avif",
-  "/game-photos/32.avif",
-  "/game-photos/33.avif",
-  "/game-photos/34.avif",
-  "/game-photos/35.avif",
-  "/game-photos/36.avif",
+  "game-photos/1.avif",
+  "game-photos/2.avif",
+  "game-photos/3.avif",
+  "game-photos/4.avif",
+  "game-photos/5.avif",
+  "game-photos/6.avif",
+  "game-photos/7.avif",
+  "game-photos/8.avif",
+  "game-photos/9.avif",
+  "game-photos/10.avif",
+  "game-photos/11.avif",
+  "game-photos/12.avif",
+  "game-photos/13.avif",
+  "game-photos/14.avif",
+  "game-photos/15.avif",
+  "game-photos/16.avif",
+  "game-photos/17.avif",
+  "game-photos/18.avif",
+  "game-photos/19.avif",
+  "game-photos/20.avif",
+  "game-photos/21.avif",
+  "game-photos/22.avif",
+  "game-photos/23.avif",
+  "game-photos/24.avif",
+  "game-photos/25.avif",
+  "game-photos/26.avif",
+  "game-photos/27.avif",
+  "game-photos/28.avif",
+  "game-photos/29.avif",
+  "game-photos/30.avif",
+  "game-photos/31.avif",
+  "game-photos/32.avif",
+  "game-photos/33.avif",
+  "game-photos/34.avif",
+  "game-photos/35.avif",
+  "game-photos/36.avif",
 ];
 
 export default function ValentinesProposal() {
@@ -135,7 +135,7 @@ export default function ValentinesProposal() {
               <span className={`inline-block text-5xl text-[#ff7f7f] mb-3.5`}>Subuuuu,</span><br/> Main tuhade naal time spend karna pasand karda haan. <br/>Ki tussi meri Valentine banogi?
             </h2>
             <Image
-              src="/sad_hamster.png"
+              src="sad_hamster.png"
               alt="Sad Hamster"
               width={200}
               height={200}
@@ -178,7 +178,7 @@ export default function ValentinesProposal() {
             Haan kehṇ layi dhannvaad… <br/>Main tuhanu bahut pyaar karda haan 💕
             <p className="text-sm mt-4">For more information, write me!!! 💌</p>
             <Image
-              src="/hamster_jumping.gif"
+              src="hamster_jumping.gif"
               alt="Hamster Feliz"
               width={200}
               height={200}
